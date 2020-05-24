1ee21d6092ff76b46e75bcfcf4cad6c50fc41e6d
chri911 <delightsome@bigmir.net>
2020-05-24 21:58:36 +0300

    -

--- js/main.js
+++ js/main.js
@@ -17,7 +17,17 @@ for (let video of videos) {
     });
 }
 
+$(".menu__list").on("click","a", function (event) {
+          event.preventDefault();
+          var id  = $(this).attr('href'),
+              top = $(id).offset().top;
+          $('body,html').animate({scrollTop: top-150}, 1500);
+      });
   
+      $(".menu ul li a").click(function(e) {
+        $(".menu ul li a").removeClass('active');
+        $(this).addClass('active');
+      })
   
   
   });
